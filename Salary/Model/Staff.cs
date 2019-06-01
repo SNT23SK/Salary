021c1cb434fa292898460e1863e05547991f2344
naf <alexnejchev73@gmail.com>
2019-06-01 15:20:27 +0400

    Added Staff

---
+++ Salary/Model/Staff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary.Model
+{
+    public class Staff
+    {
+        public int staff_id { get; set; }
+        public string staff_name { get; set; }
+        public int cases_with_first_reply { get; set; }
+        public int first_reply_speed { get; set; }
+        public int total_amount_of_replies { get; set; }
+        public int closed_cases { get; set; }
+        public int closing_speed { get; set; }
+        public int average_rating { get; set; }
+    }
+}
